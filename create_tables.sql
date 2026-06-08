22d7952c966660c1f1ead404c0b40f4234ec99e5
Felipe Salgueiro <felipesalgueiro1@gmail.com>
2026-06-08 17:09:27 -0300

    Cria estrutura inicial do banco PostgreSQL

---
+++ create_tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE exames_realizados (
+
+    unidade VARCHAR(20),
+
+    nome VARCHAR(255),
+
+    funcao VARCHAR(255),
+
+    setor VARCHAR(100),
+
+    exame VARCHAR(255),
+
+    data_exame DATE
+);
